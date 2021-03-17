ba14c300df5e17bff516f68db1ec78247fc5d0ef
Aleksandr <a.turovets90@gmail.com>
2021-03-17 15:07:12 +0300

    finished management

--- js/main.js
+++ js/main.js
@@ -16,6 +16,34 @@ $(document).ready(function(){
     });
 
 
+    $('.nav_tabs li ').each(function(){
+        var tab_link=$(this).index();
+        var tab_content=$('.tab_content .tab-pane').index();
+        $(this).click(function(){
+            console.log(tab_link);
+            $('.nav_tabs li ').removeClass('act');
+            $('.tab_content .tab_pane').removeClass('active');
+            $(this).addClass('act');
+            $('.tab_content .tab_pane').eq(tab_link).addClass('active');
+        });
+    });
+
+    $('.game_item_box').each(function(){
+        var game_item_box=$(this);
+        var game_item_toggle=$(this).find('.game_item_toggle')
+        var game_item_body=$(this).find('.game_item_body');
+        $(game_item_toggle).click(function(){
+            if($(game_item_body).is(':visible')){
+                $(game_item_box).removeClass('active');
+                $(game_item_body).slideUp();
+                $(this).text('Change');
+            }else{
+                $(game_item_box).addClass('active');
+                $(game_item_body).slideDown();
+                $(this).text('Cancel');
+            }
+        });
+    });
 
 });
 
